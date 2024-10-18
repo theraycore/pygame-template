54d6e4cd79d7ff7966e95362d38fb9b872024eb9
theraycore <raymundo.host@gmail.com>
2024-10-17 22:55:38 -0600

    :wrench: init loop settings

--- src/config.py
+++ src/config.py
@@ -3,6 +3,7 @@ from pydantic_settings import BaseSettings
 class Config(BaseSettings):
     SCREEN_WIDTH: int = 800
     SCREEN_HEIGHT: int = 600
+    FPS: int = 60
 
     class Config:
         env_file = ".env"
--- src/main.py
+++ src/main.py
@@ -5,9 +5,14 @@ pygame.init()
 screen = pygame.display.set_mode(
     (config.SCREEN_WIDTH, config.SCREEN_HEIGHT)
 )
+clock = pygame.time.Clock()
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+
+    screen.fill("magenta")
+    pygame.display.flip()
+    clock.tick(config.FPS)
